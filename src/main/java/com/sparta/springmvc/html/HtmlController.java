010c0a557cdd4cb3459710cd3f7e5ef9babc1877
tesbro1194 <tes1194@naver.com>
2024-09-30 14:56:59 +0900

    동적 페이지 처리 과정 추가

--- src/main/java/com/sparta/springmvc/html/HtmlController.java
+++ src/main/java/com/sparta/springmvc/html/HtmlController.java
@@ -3,6 +3,7 @@ package com.sparta.springmvc.html;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.Thymeleaf;
 
 @Controller
 public class HtmlController {
@@ -40,7 +41,15 @@ public class HtmlController {
 
 }
 
-//spring.thymeleaf.prefix=classpath:/templates/
-//
-//spring.thymeleaf.cache=false
-//spring.thymeleaf.check-template-location=true
+/*
+ 동적 페이지 처리 과정
+ 1. Client 의 요청을 **Controller**에서 **Model** 로 처리합니다.
+    1. DB 조회가 필요하다면 DB 작업 후 처리한 데이터를 **Model**에 저장합니다.
+ 2. Template engine(Thymeleaf) 에게 **View**, **Model** 전달합니다.
+    1. **View**: 동적 HTML 파일
+    2. **Model**: View 에 적용할 정보들
+ 3. Template engine
+    1. **View**에 **Model**을 적용 → 동적 웹페이지 생성
+        1. 예) 로그인 성공 시, "로그인된 사용자의 Nickname"을 페이지에 추가
+        2. Template engine 종류: 타임리프(Thymeleaf), Groovy, FreeMarker, Jade, JSP 등
+ 4. Client(브라우저)에게 **View**(동적 웹 페이지, HTML)를 전달 해줍니다.*/
