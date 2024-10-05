8277810db68de0b249d6889e7efb734a599a94cc
tesbro1194 <tes1194@naver.com>
2024-10-05 22:17:34 +0900

    주석 추가

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -17,7 +17,7 @@ public class RequestController {
     @GetMapping("/star/{name}/age/{age}")  // URL 경로에서 데이터를 받고자 하는 위치의 경로에 {data} 중괄호를 사용
     @ResponseBody
     //해당 요청 메서드 파라미터에 @PathVariable 애너테이션과 함께 {name} 중괄호에 선언한 변수명과 변수타입을 선언하면 해당 경로의 데이터를 받아올 수 있다.
-    public String helloRequestPath(@PathVariable String name, @PathVariable(required = false) int age) // (required = false) 클라이언트가 보내는 age 가 없어도 받을 수 있음. 초기값이 들어감.
+    public String helloRequestPath(@PathVariable String name, @PathVariable(required = false) int age)
     {
         return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
     }
@@ -26,21 +26,27 @@ public class RequestController {
     // [Request sample] GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95   param 이후 ? , 추가, 데이터 구분은 &
     @GetMapping("/form/param")
     @ResponseBody
-    public String helloGetRequestParam(@RequestParam String name, int age) {  // @RequestParam 생략 가능, simple value 타입일 때
+    public String helloGetRequestParam(@RequestParam String name, int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
+    // @PathVariable, @RequestParam 생략 가능, simple value 타입일 때
+    // (required = false) 클라이언트가 보내는 age 가 없어도 받을 수 있음. 초기값이 true 로 들어감.
 
-/*  클라이언트에서 서버로, Post 방식으로 넘어오는 데이터를 받는 방법. payload 에 담겨서 넘어옴.
+
+/*  클라이언트에서 서버로, Post 방식으로 넘어오는 데이터를 받는 방법. payload 에 담겨서 넘어옴. form 태그!
     [Request sample] : POST http://localhost:8080/hello/request/form/param
     Header
      Content type: application/x-www-form-urlencoded
     Body
-     name=Robbie&age=95    query String 표현이다.                 */
+     name=Rob&age=30   query String 표현이다.                 */
     @PostMapping("/form/param")
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
+
+
+
 
     // HTTP 데이터를 객체로 처리하는 방법 : Setter 메서드, 혹은 입력을 받을 수 있는 생성자 메서드가 필요함.
 
@@ -48,7 +54,7 @@ public class RequestController {
      Header
       Content type: application/x-www-form-urlencoded
      Body
-      name=Robbie&age=95*/    // Body 에 query String 으로 넘어온 것을 객체로 !
+      name=Robbie&age=95*/    // Body 에 query String 으로 넘어온 것을 객체로 ! @ModelAttribute
     @PostMapping("/form/model")
     @ResponseBody
     public String helloRequestBodyForm(@ModelAttribute Star star) {
@@ -56,8 +62,7 @@ public class RequestController {
     }
 
     //  2. [Request sample] : GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
-    // `?name=Robbie&age=95` 이런 방식으로 넘어온 데이터를 객체로 !
-
+    // `?name=Robbie&age=95` 이런 방식으로 넘어온 데이터를 객체로 ! @ModelAttribute
     @GetMapping("/form/param/model")
     @ResponseBody
     public String helloRequestParam(@ModelAttribute Star star) {  // @ModelAttribute 생략 가능 , 객체일 때
